f40e58dfaa724c7cab9ce884d40d988422ab5a19
Anton Bukov <k06aaa@gmail.com>
2018-10-02 13:05:20 +0300

    Some fixes

--- contracts/Donatocracy.sol
+++ contracts/Donatocracy.sol
@@ -7,6 +7,7 @@ import "./lib/Set.sol";
 
 contract Donatocracy is Ownable {
     using Set for Set.Data;
+
     uint256 public minReward = 10 finney;
 
     struct Proposal {
@@ -15,11 +16,16 @@ contract Donatocracy is Ownable {
     }
 
     Proposal[] public proposals;
-    Set.Data pendingProposalIndicies;
+    Set.Data internal pendingProposalIndicies;
 
-    event ProposalRewardCreate(uint index, uint256 value);
-    event ProposalRewardUpdate(uint index, uint256 value);
+    event ProposalCreated(uint index, uint256 value);
+    event ProposalVoted(uint index, uint256 value);
+    event ProposalMerged(uint index, uint duplicateIndex);
     event ProposalAccepted(uint index, uint256 value);
+
+    function proposalsCount() public view returns(uint) {
+        return proposals.length;
+    }
 
     function allProposals() public view returns(string concatenatedTexts, uint[] starts) {
         bytes memory result;
@@ -42,6 +48,7 @@ contract Donatocracy is Ownable {
     function createProposal(string _proposal) public payable {
         require(msg.value >= minReward, "Proposal creation requires more value");
 
+        emit ProposalCreated(proposals.length, msg.value);
         pendingProposalIndicies.add(proposals.length);
         proposals.push(Proposal({
             text: _proposal,
@@ -52,11 +59,21 @@ contract Donatocracy is Ownable {
     function voteForProposal(uint _proposalIndex) public payable {
         require(pendingProposalIndicies.contains(_proposalIndex), "Proposal do not exist");
         proposals[_proposalIndex].reward += msg.value;
+        emit ProposalVoted(_proposalIndex, msg.value);
     }
 
     function acceptProposal(uint _proposalIndex) public onlyOwner {
         require(pendingProposalIndicies.remove(_proposalIndex), "Proposal does not exist");
         msg.sender.transfer(proposals[_proposalIndex].reward);
+    }
+
+    function mergerProposal(uint _proposalIndex, uint _duplicateProposalIndex, string _newText) public onlyOwner {
+        require(pendingProposalIndicies.contains(_proposalIndex), "Proposal does not exist");
+        require(pendingProposalIndicies.remove(_duplicateProposalIndex), "Proposal does not exist");
+        proposals[_proposalIndex].reward += proposals[_duplicateProposalIndex].reward;
+        proposals[_duplicateProposalIndex].reward = 0;
+        proposals[_proposalIndex].text = _newText;
+        emit ProposalMerged(_proposalIndex, _duplicateProposalIndex);
     }
 
     function bestProposal() public view returns(uint bestProposalIndex, uint256 bestProposalReward) {
